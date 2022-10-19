1d1f913a5b0e78bb4ec586e51eea15449916e3c4
Enio Schaefer <enio100schaefer@hotmail.com>
2022-10-19 16:13:01 -0300

    [Deploy] :rocket: Requisito 04 concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Testa o componente NotFound ->', () => {
+  test('Testa se o componente NotFound possui um heading com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Testa se o componente NotFound possui uma imagem com src correto', () => {
+    render(<NotFound />);
+
+    const notFoundImage = screen.getByRole('img');
+
+    expect(notFoundImage).toBeInTheDocument();
+    expect(notFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
