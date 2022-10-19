612a8e07a6c443ec8d4b71f0878f05ebdec0d178
Enio Schaefer <enio100schaefer@hotmail.com>
2022-10-19 16:05:08 -0300

    [Deploy] :rocket: Requisito 03 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa se e exibida a mensagem No favorite pokemon found caso nao haja pokemons favoritados', () => {
+  renderWithRouter(<App />);
+
+  const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  userEvent.click(favoritePokemonsLink);
+
+  const noPokemonFound = screen.getByText('No favorite pokemon found');
+
+  expect(noPokemonFound).toBeInTheDocument();
+});
+
+test('Testa se sao exibidos todos os pokemons favoritados', () => {
+  renderWithRouter(<App />);
+
+  const details = screen.getByRole('link', { name: 'More details' });
+
+  userEvent.click(details);
+
+  const favoriteCheckbox = screen.getByRole('checkbox');
+  const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  userEvent.click(favoriteCheckbox);
+  userEvent.click(favoritePokemonsLink);
+
+  const pokemonName = screen.getByTestId('pokemon-name');
+
+  expect(pokemonName).toBeInTheDocument();
+});
