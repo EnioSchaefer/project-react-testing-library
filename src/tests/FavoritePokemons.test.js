ba4df2e4669e6bfa6e2c0df6fc7ba014afe48fe7
Enio Schaefer <enio100schaefer@hotmail.com>
2022-10-19 16:08:14 -0300

    [Fix] :bug: Adicionando describe no teste de FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,30 +3,32 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-test('Testa se e exibida a mensagem No favorite pokemon found caso nao haja pokemons favoritados', () => {
-  renderWithRouter(<App />);
+describe('Testa o componente FavoritePokemons ->', () => {
+  test('Testa se e exibida a mensagem No favorite pokemon found caso nao haja pokemons favoritados', () => {
+    renderWithRouter(<App />);
 
-  const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
-  userEvent.click(favoritePokemonsLink);
+    const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoritePokemonsLink);
 
-  const noPokemonFound = screen.getByText('No favorite pokemon found');
+    const noPokemonFound = screen.getByText('No favorite pokemon found');
 
-  expect(noPokemonFound).toBeInTheDocument();
-});
+    expect(noPokemonFound).toBeInTheDocument();
+  });
 
-test('Testa se sao exibidos todos os pokemons favoritados', () => {
-  renderWithRouter(<App />);
+  test('Testa se sao exibidos todos os pokemons favoritados', () => {
+    renderWithRouter(<App />);
 
-  const details = screen.getByRole('link', { name: 'More details' });
+    const details = screen.getByRole('link', { name: 'More details' });
 
-  userEvent.click(details);
+    userEvent.click(details);
 
-  const favoriteCheckbox = screen.getByRole('checkbox');
-  const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
-  userEvent.click(favoriteCheckbox);
-  userEvent.click(favoritePokemonsLink);
+    const favoriteCheckbox = screen.getByRole('checkbox');
+    const favoritePokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoriteCheckbox);
+    userEvent.click(favoritePokemonsLink);
 
-  const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonName = screen.getByTestId('pokemon-name');
 
-  expect(pokemonName).toBeInTheDocument();
+    expect(pokemonName).toBeInTheDocument();
+  });
 });
