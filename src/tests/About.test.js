3920249d6ad804b5218f965342ecb0fcf29fd0de
Enio Schaefer <enio100schaefer@hotmail.com>
2022-10-19 15:41:36 -0300

    [Deploy] :rocket: Requisito 02 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import { About } from '../pages';
+
+test('Testa se a pagina contem a descricao da pokedex', () => {
+  render(<About />);
+
+  const info01 = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
+  const info02 = screen.getByText('One can filter Pokémons by type, and see more details for each one of them');
+
+  expect(info01).toBeInTheDocument();
+  expect(info02).toBeInTheDocument();
+});
+
+test('Testa se a pagina contem um heading com o texto About Pokedex', () => {
+  render(<About />);
+
+  const heading = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+
+  expect(heading).toBeInTheDocument();
+
+  const info01 = screen.getByText('This application simulates a Pokédex, a digital encyclopedia containing all Pokémons');
+  const info02 = screen.getByText('One can filter Pokémons by type, and see more details for each one of them');
+
+  expect(info01).toBeInTheDocument();
+  expect(info02).toBeInTheDocument();
+
+  const aboutImage = screen.getByRole('img');
+
+  expect(aboutImage.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
